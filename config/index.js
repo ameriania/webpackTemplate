daf9ff284ad1bee7f6907373a72f1126bedafd5e
fengqinglingyu <liang823740098@hotmail.com>
2018-04-02 17:22:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Dev/EPF/Web/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
